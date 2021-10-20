925f5d64e9b55fb3904604e1578626fc1f4e7c0d
Ashwin Ramgopal <38460932+ashwin2000@users.noreply.github.com>
2021-10-20 14:08:18 +0530

    Create messaging.py

---
+++ src/messaging.py
@@ -0,0 +1,6 @@
+from . import User
+ 
+class Messaging:
+   #array of user names
+ 
+   #client server architecture for implementation of messaging through the existing connections running parallel to the game
