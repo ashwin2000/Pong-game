f21f894045dbe254986d28764589014ee7281d4b
Ashwin Ramgopal <38460932+ashwin2000@users.noreply.github.com>
2021-10-20 14:13:03 +0530

    Create client.py

---
+++ client.py
@@ -0,0 +1,8 @@
+import math, sys, socket
+from lib import window
+from lib import settings
+import pyglet
+ 
+mainwindow.MainWindow(width=settings.WINDOW_WIDTH, height=settings.WINDOW_HEIGHT)
+pyglet.app.run()
+ 
